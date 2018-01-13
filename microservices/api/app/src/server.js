d923ba030d66e18d8543955e5450fedb282b205d
addypt <>
2018-01-13 13:09:23 +0530

    Added axios to express app

--- microservices/api/app/src/server.js
+++ microservices/api/app/src/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');ha
 
 //your routes here
 app.get('/', function (req, res) {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
+
+app.get('/signup', function (req, res){
+  console.log(req);
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
